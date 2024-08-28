6196fd2940680074c2262312047d8075df14f611
Elekoumpo <emezit1@etf.unsa.ba>
2024-08-28 15:35:36 +0200

    Communication between Order and Inventory Service-modified Inventory Service in a way that it returns list of products by skuCode

--- order-service/src/main/java/com/ejubmicro/order_service/service/OrderService.java
+++ order-service/src/main/java/com/ejubmicro/order_service/service/OrderService.java
@@ -8,6 +8,7 @@ import com.ejubmicro.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -29,6 +31,19 @@ public class OrderService {
         order.setOrderLineItemsList(orderLineItems);
 
         //Call inventory service (WebClient chosen over RestTemplate) - place order if product is in stock
+            Boolean result = webClient.get() //in Inventory Controller I mapped the Get
+                    .uri("http://localhost:8082/api/inventory")
+                    .retrieve()
+                    .bodyToMono(Boolean.class) //by default client will make async request, but I need to make it sync
+                    .block();
+
+        if(result) {
+        orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
+
+
         orderRepository.save(order);
     }
 
